b221aad55749131b588b0eb26d5a6e6577387d36
BigHead <59408273+bwjin310@users.noreply.github.com>
2023-08-21 09:11:20 +0800

    cmake build file added

---
+++ build/CMakeFiles/mavsdk_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mavsdk_test.dir/main.cpp.o"
+  "mavsdk_test"
+  "mavsdk_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mavsdk_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
